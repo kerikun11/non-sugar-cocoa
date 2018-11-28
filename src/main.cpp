19ec1119da48a15c47fc5018b98e18a760c66c23
Ryotaro Onuki <kerikun11+github@gmail.com>
2018-11-28 14:51:41 +0900

    ベースとなるPlatformIOプロジェクトを追加 #3
    中身は空っぽ

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <esp32-hal-log.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  log_i("Hello World!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
