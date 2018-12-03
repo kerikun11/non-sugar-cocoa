dbb124bcc6adfe35e2ab418092353cd3d7044b5b
Ryotaro Onuki <kerikun11+github@gmail.com>
2018-12-04 01:10:44 +0900

    コードフォーマットを調整

--- src/scene/scene_clock.hpp
+++ src/scene/scene_clock.hpp
@@ -7,7 +7,8 @@
  */
 #pragma once
 
-#include<string>
+#include <string>
+
 #include "hardware/hardware.h"
 #include "scene/event.hpp"
 #include "scene/scene.hpp"
@@ -63,47 +64,46 @@ public:
 
 protected:
   std::shared_ptr<hardware::Hardware> m_hardware;
-  
+
   void updateDisplayClock(bool clean = false) {
     //アニメーションのための数値
-    static uint8_t frame=0;
+    static uint8_t frame = 0;
     frame++;
     //現在時刻の取得
-    int hh = 0, mm = 0, ss = 0,ms=0;
+    int hh = 0, mm = 0, ss = 0, ms = 0;
     struct tm timeinfo;
     if (getLocalTime(&timeinfo)) {
       hh = timeinfo.tm_hour;
       mm = timeinfo.tm_min;
       ss = timeinfo.tm_sec;
-      auto now=std::chrono::system_clock::now().time_since_epoch();
-      ms=std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
+      auto now = std::chrono::system_clock::now().time_since_epoch();
+      ms = std::chrono::duration_cast<std::chrono::milliseconds>(now).count();
     }
     //時刻描画
-    displayClock(clean,hh,mm,ss,ms);
+    displayClock(clean, hh, mm, ss, ms);
     //ボタン説明の描画
-    const int tMax=10,height=3,topY=200;
-    const int t=frame%tMax;
-    int y;//アニメーションのためのy方向変位
-    if(t<tMax/2){
-      y=topY-t*(t-tMax/2)/(tMax*tMax/16/height)+height;
-    }else{
-      y=topY+(t-tMax/2)*(t-tMax)/(tMax*tMax/16/height)+height;
+    const int tMax = 10, height = 3, topY = 200;
+    const int t = frame % tMax;
+    int y; //アニメーションのためのy方向変位
+    if (t < tMax / 2) {
+      y = topY - t * (t - tMax / 2) / (tMax * tMax / 16 / height) + height;
+    } else {
+      y = topY + (t - tMax / 2) * (t - tMax) / (tMax * tMax / 16 / height) +
+          height;
     }
     M5.Lcd.setTextColor(TFT_PINK, TFT_BLACK);
-    M5.Lcd.fillRect(42,topY,50,y-topY,TFT_BLACK);//文字を消す
-    M5.Lcd.drawString("SET",42,y,4);//フォントサイズ4で3文字の描画を左ボタンの上にするなら、(42,205)に描画
-
-
+    M5.Lcd.fillRect(42, topY, 50, y - topY, TFT_BLACK); //文字を消す
+    M5.Lcd.drawString(
+        "SET", 42, y,
+        4); //フォントサイズ4で3文字の描画を左ボタンの上にするなら、(42,205)に描画
   }
 
-  void displayClock(bool clean,int hh,int mm,int ss,int ms)const{
+  void displayClock(bool clean, int hh, int mm, int ss, int ms) const {
     //直前に描画を行った分・秒の値。これが現在時刻と異なっていたら描画する。分・秒が絶対に取りえない値で初期化することで、初めてのdrawClock()では描画処理が必ず発生するようにする
-    static byte
-        omm = 99,
-        oss = 99; 
+    static byte omm = 99, oss = 99;
     static byte xcolon = 0, xsecs = 0;
     // 明るく描画するか
-    static bool lightColon=true;//500msごとに切り替える
+    static bool lightColon = true; // 500msごとに切り替える
 
     //描画準備
     if (clean) {
@@ -123,7 +123,8 @@ protected:
       omm = mm;
       //時間の描画、0でいいので2桁目を描画する
       if (hh < 10) {
-        xpos += M5.Lcd.drawChar('0', xpos, ypos,8); // Add hours leading zero for 24 hr clock
+        xpos += M5.Lcd.drawChar('0', xpos, ypos,
+                                8); // Add hours leading zero for 24 hr clock
       }
       xpos += M5.Lcd.drawNumber(hh, xpos, ypos, 8); // Draw hours
       xcolon = xpos; //秒の描画において":"を描画するのに用いる
@@ -138,13 +139,13 @@ protected:
       xsecs = xpos; // Sae seconds 'x' position for later display updates
     }
     //毎秒の秒の描画
-    if(clean || lightColon!=(ms%1000<500)){
+    if (clean || lightColon != (ms % 1000 < 500)) {
       // ossの更新をし、秒の値が変わっていないうちは描画処理を行わないようにする
       oss = ss;
       xpos = xsecs;
-      lightColon=(ms%1000<500);
+      lightColon = (ms % 1000 < 500);
       //":"の描画
-      if(!lightColon){
+      if (!lightColon) {
         // lightColonを見て：の明るさの描画を決める
         M5.Lcd.setTextColor(0x39C4, TFT_BLACK); //文字色を暗くする
         M5.Lcd.drawChar(
