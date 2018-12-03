7ece430d4803c151ee40dba0c9f6c28094182bc0
YOSHIOKA Takuma <lo48576@hard-wi.red>
2018-12-03 14:46:28 +0900

    Send tick event to scene manager

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <freertos/queue.h>
 
 #include "hardware/hardware.h"
+#include "scene/event.hpp"
 #include "scene/scene_manager.hpp"
 
 std::shared_ptr<hardware::Hardware> hw;
@@ -21,12 +22,23 @@ void setup() {
   hw = std::make_shared<hardware::Hardware>();
   hw->begin();
 
+  // シーン管理機構を初期化。
   log_i("Scene manager event queue size = %d", EVENT_QUEUE_LENGTH);
   QueueHandle_t queueToSceneManager =
       xQueueCreate(EVENT_QUEUE_LENGTH, sizeof(scene::Event *));
   scene_manager.initialize(hw, queueToSceneManager);
+
+  // ハードウェア関係の設定。
+  hw->onTickEvent([=]() {
+    scene::Event *ev = new scene::Event{scene::EventKind::Tick};
+    xQueueSendToBack(queueToSceneManager, ev, 0);
+  });
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  size_t numEventsProcessed = scene_manager.processExternalEvents();
+  if (numEventsProcessed != 0) {
+    log_i("Processed %zd external events by scene manager", numEventsProcessed);
+  }
 }
